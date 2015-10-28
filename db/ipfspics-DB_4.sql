4d15e141e7559cf83109a052bfdeab9d1afde795
didiercf <didiercamous@gmail.com>
2015-10-28 19:05:52 -0300

    DB structure
    All the queries needed to create the ipfs.pifs database

---
+++ db/ipfspics-DB_4.sql
@@ -0,0 +1,7 @@
+CREATE TABLE votes (hash VARCHAR(46),
+                    vote_type TINYTEXT,
+                    ip VARCHAR(45) NOT NULL,
+                    timestamp INT(11) NOT NULL,
+
+                    PRIMARY KEY (hash)
+);
