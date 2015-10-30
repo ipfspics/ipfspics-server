820c0753eab1623eb3047ba284a41f9a494306cd
Vincent Cloutier <vincent1cloutier@gmail.com>
2015-10-29 20:13:20 -0400

    Started php ipfs api class #12 (also helps for #10 and #19

---
+++ class/ipfs.class.php
@@ -0,0 +1,70 @@
+<?php
+/*
+    IPFS api binding in PHP
+    Copyright (C) 2015 IpfsPics 2015
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+class IPFS {
+	private $gatewayIP;
+	private $gatewayPort;
+	private $gatewayApiPort;
+
+	function __construct($ip, $port, $apiPort) {
+		$this->gatewayIP      = $ip;
+		$this->gatewayPort    = $port;
+		$this->gatewayApiPort = $apiPort;
+	}
+
+	public function cat ($hash) {
+		$ip = $this->gatewayIP;
+		$port = $this->gatewayPort;
+		return $this->curl("http://$ip:$port/ipfs/$hash"); 
+
+	}
+
+	public function add ($content) {
+
+	}
+
+	public function ls ($hash) {
+
+	}
+
+	public function size ($hash) {
+
+	}
+
+	private function curl ($url) {
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+		 
+		$output = curl_exec($ch);
+
+		if ($output == FALSE) {
+			//todo: when ipfs doesn't answer
+		}		 
+		curl_close($ch);
+ 
+
+		return $output;
+	}
+}
+
+
