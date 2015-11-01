be03e5819902257161bebf3af34a83d60574864f
didiercf <didiercamous@gmail.com>
2015-11-01 16:45:08 -0500

    Removed comma

--- db/ipfspics-DB_4.sql
+++ db/ipfspics-DB_4.sql
@@ -1,5 +1,5 @@
 CREATE TABLE votes (hash VARCHAR(46),
                     vote_type TINYTEXT,
                     ip VARCHAR(45) NOT NULL,
-                    timestamp INT(11) NOT NULL,
+                    timestamp INT(11) NOT NULL
 );
