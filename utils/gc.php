c8eb1955816dea91d5bc1b2fd106b4b31154dafe
Vincent Cloutier <vincent@cloutier.co>
2018-05-20 13:55:33 -0400

    updated gc.php

--- utils/gc.php
+++ utils/gc.php
@@ -19,52 +19,24 @@
 error_reporting(1);
 
 require __DIR__ . '/../vendor/autoload.php';
-include __DIR__ ."/../pswd.php";
 
 use Cloutier\PhpIpfsApi\IPFS;
 
 print("gc \n \n");
 
-$db = new PDO('mysql:host=localhost;dbname=hashes;charset=utf8', $db_user, $db_pswd);
+if (getenv('IPFSPICS_DB') != "") {
+	        $mongo = new MongoDB\Client(getenv('IPFSPICS_DB'));
+} else {
+	        $mongo = new MongoDB\Client("mongodb://localhost:27017");
+}
+$db = $mongo->ipfspics;
 $ipfs = new IPFS("localhost", "8080", "5001");
 
-// Makes sure all the banned hashes are removed
+$toUnpin = $db->hashes->find(['gcloud.adult' => "VERY_LIKELY"]);
 
-if (!in_array("skipUnpin", $argv) ) {
-	$bannedHashes = $db->query("SElECT * FROM hash_info WHERE banned = 1;")->fetchAll();
-
-	foreach($bannedHashes as $i) {
-		$hash = $i['hash'];
-		$ipfs->pinRm($hash);
-		print "Removed banned hash: $hash \n";
-	}
+foreach($toUnpin as $i) {
+	$hash = $i['hash'];
+	$ipfs->pinRm($hash);
+	echo "unpined $hash\n";
 }
 
-if (!in_array("skipPin", $argv) ) {
-	$notBannedHashes = $db->query("SElECT * FROM hash_info WHERE banned != 1;")->fetchAll();
-
-	foreach($notBannedHashes as $i) {
-		$hash = $i['hash'];
-		$ipfs->pinAdd($hash);
-		print("Pinned hash: $hash \n");
-	}
-} 
-
-// This section if to optionnaly unpin porn on your instance. 
-// You have to add "removeSomeNSFW" as an argument when running gc.php.
-// For now, it removes NSFW pictures which did not get votes and are more than two weeks old. 
-// If you want to keep them, make sure they are replicated on another computer before running this.
-
-if (in_array("removeSomeNSFW", $argv) ) {
-
-	$NSFWtoRemove = $db->query("SELECT hash as p_hash FROM hash_info WHERE nsfw = 1 AND first_seen < UNIX_TIMESTAMP() - 1209600 HAVING (SELECT COUNT(*) FROM votes WHERE hash = p_hash) < 2;")->fetchAll();
-
-	foreach($NSFWtoRemove as $i) {
-		$hash = $i['p_hash'];
-		$ipfs->pinRm($hash);
-		print "Removed randomly: $hash \n";
-
-	}
-} 
-
-print "You can now run ipfs built-in garbage collector \n";
